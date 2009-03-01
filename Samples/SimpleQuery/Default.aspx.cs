4481f2e7bae1000fd44297d8916ec41fbbd74851
SND\danelkhen_cp <SND\danelkhen_cp@9b283d60-5439-405e-af05-b73fd8c4d996>
2009-03-01 10:17:13 +0000

    git-svn-id: https://facebooklinq.svn.codeplex.com/svn@13544 9b283d60-5439-405e-af05-b73fd8c4d996

---
+++ Samples/SimpleQuery/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using facebook.Web;
+
+namespace facebook.Linq.Demo
+{
+	public partial class _Default : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			if (FacebookContext.Current.TryAuthenticating(true))
+			{
+				var db = new FacebookDataContext();
+
+				var useQuerySyntax = true;
+				var useMethodSyntax = false;
+
+				if (useQuerySyntax)
+				{
+					var friendIds = from friend in db.friend_info where friend.uid1 == db.uid select friend.uid2;
+					var friendDetails = (from user in db.user where friendIds.Contains(user.uid) select new { Name = user.name, Picture = user.pic_small }).Take(5);
+					listFriends.DataSource = friendDetails.ToArray();
+					DataBind();
+				}
+				else if (useMethodSyntax)
+				{
+					var friendIds2 = db.friend_info.Where(t => t.uid1 == db.uid).Select(t => t.uid2);
+					var friendDetails2 = db.user.Where(t => friendIds2.Contains(t.uid)).Select(t => new { Name = t.name, Picture = t.pic_small }).Take(5);
+					listFriends.DataSource = friendDetails2.ToArray();
+					DataBind();
+				}
+			}
+		}
+	}
+}
---
+++ Source/facebook.Linq/Web/FacebookCallback.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.SessionState;
+
+namespace facebook.Web
+{
+	/// <summary>
+	/// Facebook callback URL should be pointed to here
+	/// </summary>
+	public class FacebookCallback : IHttpHandler, IRequiresSessionState
+	{
+		public void ProcessRequest(HttpContext context)
+		{
+			var fc = FacebookContext.Get(context);
+			var session = fc.Session;
+			var authenticated = session.IsAuthenticated;
+			if (!authenticated)
+				authenticated = session.TryAuthenticating(context.Request);
+			if (authenticated)
+			{
+				var page = Facebook.DefaultPage ?? "./?";
+				if (page.Contains("?"))
+					page += "&";
+				else
+					page += "?";
+				page += HttpUtility.UrlDecode(context.Request.QueryString.ToString());
+				context.Response.Redirect(page);
+			}
+			else
+			{
+				if (context.Request["app"].IsNotNullOrEmpty())
+				{
+					var appName = context.Request["app"];
+					if (appName.Contains("?"))
+						appName = appName.Substring(0, appName.IndexOf('?'));
+					var requestUrl = context.Request.Url;
+					var redirectedPageQueryString = HttpUtility.UrlEncode("?" + HttpUtility.UrlDecode(context.Request.QueryString.ToString()).Replace('?', '&'));
+					var url = String.Format("http://apps.facebook.com/{0}/?redirectTo=http://{1}{2}{3}", appName, requestUrl.Host, requestUrl.AbsolutePath, redirectedPageQueryString);
+					fc.RedirectTopFrame(url);
+				}
+				else if (Facebook.HasValidConfiguration)
+				{
+					fc.RedirectTopFrame(Facebook.FacebookLoginUrl);
+				}
+				else
+				{
+					context.Response.Write("Facebook configuration is missing. (facebook.Linq.APIKey, facebook.Linq.Secret, facebook.Linq.ApplicationID)");
+				}
+			}
+		}
+
+
+
+		public bool IsReusable
+		{
+			get
+			{
+				return false;
+			}
+		}
+	}
+}
